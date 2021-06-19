dd0fee3134a266405b1a051c7f8c3ecf8a5d31b9
omerf <omer.farukdursun@outlook.com>
2021-06-19 10:06:18 +0300

    Add Constructor methods to classes

--- src/omerfaruk/dursun/coffe/shop/application/Material.java
+++ src/omerfaruk/dursun/coffe/shop/application/Material.java
@@ -1,8 +1,19 @@
 package omerfaruk.dursun.coffe.shop.application;
-
+/**
+ * The Material class represents the ingredients that make up the different kinds of coffee.
+ * @author Omer Faruk Dursun
+ */
 public class Material {
 
     private String name;
+
+    /**
+     * Material Constructor with name field
+     * @param name Materials name
+     */
+    public Material(String name) {
+        this.name = name;
+    }
 
     public String getName() {
         return name;
--- src/omerfaruk/dursun/coffe/shop/application/MaterialQuantity.java
+++ src/omerfaruk/dursun/coffe/shop/application/MaterialQuantity.java
@@ -1,7 +1,27 @@
 package omerfaruk.dursun.coffe.shop.application;
-
+/**
+ * The Material class represents a material and quantity of that material.
+ * @author Omer Faruk Dursun
+ */
 public class MaterialQuantity {
     private Material material;
     private Integer quantity;
 
+    /**
+     * Material Constructor with material and quantity fields
+     * @param material A material object
+     * @param quantity Quantity of the material object
+     */
+    public MaterialQuantity(Material material, Integer quantity) {
+        this.material = material;
+        this.quantity = quantity;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
 }
