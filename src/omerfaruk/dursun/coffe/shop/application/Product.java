b462e12d8cc33d15263f245c58e33d13acb4686f
omerf <omer.farukdursun@outlook.com>
2021-06-19 10:28:09 +0300

    Override toString method of Product class - (requirement #3)

--- src/omerfaruk/dursun/coffe/shop/application/Product.java
+++ src/omerfaruk/dursun/coffe/shop/application/Product.java
@@ -2,11 +2,21 @@ package omerfaruk.dursun.coffe.shop.application;
 
 import java.util.ArrayList;
 
+/**
+ * Product class represents the items on the menu
+ * @author Omer Faruk Dursun
+ */
 public class Product {
     private String productName;
     private Double price;
     private ArrayList<MaterialQuantity> recipe;
 
+    /**
+     * Product Constructor with all fields as parameters
+     * @param productName Name of the Product
+     * @param price Price value of the Product
+     * @param recipe Recipe is an ArrayList that holds MaterialQuantity objects
+     */
     public Product(String productName, Double price, ArrayList<MaterialQuantity> recipe) {
         this.productName = productName;
         this.price = price;
@@ -35,6 +45,20 @@ public class Product {
 
     public void setRecipe(ArrayList<MaterialQuantity> recipe) {
         this.recipe = recipe;
+    }
+
+    /**
+     * Returns a formatted string that displays Product name, materials and the quantity of those materials
+     * @return the formatted String
+     */
+    @Override
+    public String toString() {
+        StringBuilder returnString = new StringBuilder(productName + " seçtiniz." + " Bu içeceğimiz ");
+        for(MaterialQuantity material :recipe){
+            returnString.append(material.getQuantity()).append(" adet ").append(material.getMaterial().getName()).append(" ");
+        }
+        returnString.append("içermektedir.");
+        return returnString.toString();
     }
 
 }
