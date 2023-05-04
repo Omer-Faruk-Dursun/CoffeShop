8e3e51ca99f82db46e6e2e05146a554c23fd169c
omerf <omerfarukdursun@outlook.com>
2023-05-04 22:09:21 +0300

    Upgrade to JUnit 5.

--- src/test/java/ProductTest.java
+++ src/test/java/ProductTest.java
@@ -1,21 +1,26 @@
 import omerfaruk.dursun.coffe.shop.application.Material;
 import omerfaruk.dursun.coffe.shop.application.MaterialQuantity;
 import omerfaruk.dursun.coffe.shop.application.Product;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
 
 import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 /**
  * ProductTest class includes unit tests for Product class
+ *
  * @author Omer Faruk Dursun
  */
 public class ProductTest {
     @Test
-    public void toStringTest(){
+    public void toStringTest() {
         ArrayList<MaterialQuantity> recipeListOfProduct = new ArrayList<>();
         recipeListOfProduct.add(new MaterialQuantity(new Material("Coffee Beans"), 7));
-        Product p = new Product("Turkish Coffee",10.0,recipeListOfProduct);
+        Product p = new Product("Turkish Coffee", 10.0, recipeListOfProduct);
         String expected = "Turkish Coffee seçtiniz. Bu içeceğimiz 7 adet Coffee Beans içermektedir.";
-        Assert.assertEquals(expected,p.toString());
+
+        assertEquals(expected, p.toString());
     }
 }
--- src/test/java/UtilityTest.java
+++ src/test/java/UtilityTest.java
@@ -1,50 +1,56 @@
 import omerfaruk.dursun.coffe.shop.application.Utility;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 /**
  * UtilityTest class includes unit tests for Utility class
+ *
  * @author Omer Faruk Dursun
  */
 public class UtilityTest {
 
     @Test
-    public void parseCoffeeNameTest(){
+    public void parseCoffeeNameTest() {
         Utility utility = new Utility();
         String returnString = utility.parseCoffeeName("Turkish Coffee (7 ₺)");
         String expected = "Turkish Coffee";
-        Assert.assertEquals(expected,returnString);
+
+        assertEquals(expected, returnString);
     }
 
     @Test
-    public void parseCoffeePriceTest(){
+    public void parseCoffeePriceTest() {
         Utility utility = new Utility();
         Double returnDouble = utility.parseCoffeePrice("Turkish Coffee (7.25 ₺)");
         Double expected = 7.25;
-        Assert.assertEquals(expected,returnDouble);
+
+        assertEquals(expected, returnDouble);
     }
 
     @Test
-    public void parseRecipeTest(){
+    public void parseRecipeTest() {
         Utility utility = new Utility();
         ArrayList<String> returnArrayList = utility.parseRecipe("4x Espresso, 12x Steamed Milk");
         ArrayList<String> expected = new ArrayList<>();
         expected.add("4x Espresso");
         expected.add("12x Steamed Milk");
-        Assert.assertEquals(expected,returnArrayList);
+
+        assertEquals(expected, returnArrayList);
 
     }
 
     @Test
-    public void parseMaterialAndQuantityTest(){
+    public void parseMaterialAndQuantityTest() {
         Utility utility = new Utility();
         ArrayList<String> returnArrayList = utility.parseMaterialAndQuantity("4x Espresso");
         ArrayList<String> expected = new ArrayList<>();
         expected.add("4");
         expected.add("Espresso");
-        Assert.assertEquals(expected,returnArrayList);
+
+        assertEquals(expected, returnArrayList);
     }
 
 }
